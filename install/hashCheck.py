999cb9e7fe739208391984f35c91d9a9dcec65a5
Gregory Green <gjggreen@gmail.com>
2018-05-17 15:14:27 +0800

    testing hash 512
    ##missing x86_32bit SHA512

--- install/hashCheck.py
+++ install/hashCheck.py
@@ -3,28 +3,28 @@ import hashlib
 
 def getHash(fileName):
     if fileName == 'elasticsearch-6.2.4.deb':
-        return 'd6beceeb93ade6c3bc18b76a7f0e365dd95f6f52'
+        return '0ccb8533af786d1659914341ab4481fef9dae99a515a9a789f128613bab42b4c3d3753a524810267e31707ef6c58e4b3c297501e8283b1da64138e9b26262119'
     elif fileName == 'kibana-6.2.4-linux-x86_64.tar.gz':
-        return '6dba24c876841fdf116a413c843f09d3e98b4002'
+        return '125c2002e1a3b197f9f0d6200f2979f2e5974a902bfec2baa4b34a0c8d55babccafdb87b8a7b69c61ca4c02b8ae97e69b9b2743f7d445f0e53e31c69288296d5'
     elif fileName == 'kibana-6.2.4-linux-x86.tar.gz':
         return '47d7707b1b8feb490276fd69b597d27af610d28b'
     elif fileName == 'logstash-6.2.4.deb':
-        return '88fbe43065cfaa6b13374f8f4a69f871b7110208'
+        return 'b30673aa23b8e150ba7136d0dd508bf1b6aa779a7a61af96279636fdc8010f0467446413b9a03591082f1d06666d6ae622ca1a409d4127a7c642fa2bb2773838'
     elif fileName == 'bro-2.5.3.tar.gz':
-        return '9c133dd3a075be1084f9bf53d79c42ddcf23633c'
+        return '45138464dcc8873409b84151a1393b7ec7206533686d31ab4a1fc1f22e83279ab105b70d8933b884526f4f411a05c0b6ca560cbdd6cdeccc05cc365604ea6210'
     return ''
 
 def checkHash(fileName):
     BUF_SIZE = 65536
-    sha1 = hashlib.sha1()
+    sha512 = hashlib.sha512()
     with open(fileName, 'rb') as file:
         while True:
             data = file.read(BUF_SIZE)
             if not data:
                 break
             sha1.update(data)
-    sha1=sha1.hexdigest()
-    if sha1 == getHash(fileName):
+    sha512=sha512.hexdigest()
+    if sha512 == getHash(fileName):
         return True
     else:
         return False
