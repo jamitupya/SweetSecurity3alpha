d968146c815c1d6a2039dcf189ebabc84eb18a7e
Gregory Green <gjggreen@gmail.com>
2018-05-17 14:49:16 +0800

    oops

--- install/packages.py
+++ install/packages.py
@@ -21,9 +21,9 @@ def install(installType):
 	if installType=='1':
 		#Full Install
 		if cpuArch.startswith('x86'):
-			os.popen('sudo apt-get -y install curl cmake g++ flex bison libpcap-dev libssl1.0-dev python-dev python-pip python-flask python-scapy apache2 libapache2-mod-wsgi swig nmap tcpdump 2>&1').read()
+			os.popen('sudo apt-get -y install curl cmake g++ flex bison libgeoip-dev libpcap-dev libssl1.0-dev python-dev python-pip python-flask python-scapy apache2 libapache2-mod-wsgi swig nmap tcpdump 2>&1').read()
 		else:
-			os.popen('sudo apt-get -y install cmake flex bison libpcap-dev libssl1.0-dev python-dev python-pip python-flask python-scapy apache2 libapache2-mod-wsgi swig ant zip git nmap tcpdump 2>&1').read()
+			os.popen('sudo apt-get -y install cmake flex bison libgeoip-dev libpcap-dev libssl1.0-dev python-dev python-pip python-flask python-scapy apache2 libapache2-mod-wsgi swig ant zip git nmap tcpdump 2>&1').read()
 			#Some ARM platforms won't have this, moving it here so it won't error on everything else
 			os.popen('sudo apt-get -y install oracle-java8-jdk 2>&1')
 		os.popen('sudo pip install elasticsearch 2>&1').read()
@@ -35,14 +35,14 @@ def install(installType):
 	elif installType=='2':
 		#Sensor Install
 		if cpuArch.startswith('x86'):
-			os.popen('sudo apt-get -y install curl cmake g++ flex bison libpcap-dev libssl1.0-dev python-dev python-pip python-scapy swig nmap tcpdump 2>&1').read()
+			os.popen('sudo apt-get -y install curl cmake g++ flex bison libgeoip-dev libpcap-dev libssl1.0-dev python-dev python-pip python-scapy swig nmap tcpdump 2>&1').read()
 		else:
-			os.popen('sudo apt-get -y install cmake flex bison libpcap-dev libssl1.0-dev python-dev python-pip python-scapy swig oracle-java8-jdk ant zip git nmap tcpdump texinfo 2>&1').read()
+			os.popen('sudo apt-get -y install cmake flex bison libpcap-dev libgeoip-dev libssl1.0-dev python-dev python-pip python-scapy swig oracle-java8-jdk ant zip git nmap tcpdump texinfo 2>&1').read()
 		os.popen('sudo pip install requests 2>&1').read()
 	elif installType=='3':
 		#Web Server Install
 		if cpuArch.startswith('x86'):
-			os.popen('sudo apt-get -y install curl python-dev python-pip python-flask python-scapy apache2 libapache2-mod-wsgi 2>&1').read()
+			os.popen('sudo apt-get -y install curl libgeoip-dev python-dev python-pip python-flask python-scapy apache2 libapache2-mod-wsgi 2>&1').read()
 		else:
 			os.popen('sudo apt-get -y install python-dev python-pip python-flask python-scapy apache2 libapache2-mod-wsgi oracle-java8-jdk 2>&1').read()
 		os.popen('sudo pip install elasticsearch 2>&1').read()
